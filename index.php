8adc31d3fd1ea656bfaf5a770467e2fa339e78e6
Reinaldy Anindya Putra <reinaldyanindya26@gmail.com>
2022-10-24 11:01:39 +0700

    add php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo "Hello World! <br>";
+
+    $nama = "reinaldy";
+    $umur = 20;
+
+    echo "Nama Saya <strong>$nama</strong>,saya berumur $umur. <br>";
+
+    $namaAdik = "rafa";
+    $umurAdik = 16;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, umur dia adalah $umurAdik tahun. <br>";
+
+    $SelisihUmur = $umur - $umurAdik;
+    $JumlahUmur = $umur + $umurAdik;
+
+    echo "Selisih usia saya dan adik saya adalah $SelisihUmur. <br>"; 
+
+    echo "Jumlah usia saya dengan adik saya adalah $JumlahUmur.";
+
+
+?>
