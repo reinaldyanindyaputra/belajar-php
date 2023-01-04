4279b4f8ff7fe78e01e86e5adbe8908fa8b976c7
Reinaldy Anindya Putra <reinaldyanindya26@gmail.com>
2023-01-04 19:40:55 +0700

    dasaran crud

--- index.php
+++ index.php
@@ -1,67 +1,212 @@
 <?php
-// 1. Buat koneksi dengan MySQL
-$con = mysqli_connect("localhost","root","","fakultas");
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "fakultas";
 
-// 2. Cek koneksi dengan MySQL
-if(mysqli_connect_errno()){
-    echo "Koneksi gagal ". mysqli_connect_error();
-}else{
-    echo "Koneksi berhasil";
+$koneksi    = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
 }
+$nim        = "";
+$nama       = "";
+$alamat     = "";
+$fakultas   = "";
+$sukses     = "";
+$error      = "";
 
-// 3. membaca data dari table mysql
-$query = "SELECT * FROM mahasiswa";
-
-// 4. tampilkan data, dengan menjalankan sql query
-$result = mysqli_query($con,$query);
-$mahasiswa = [];
-if($result){
-    //tampilkan data satuu per satu
-    while($row = mysqli_fetch_assoc($result)){
-        $mahasiswa[] = $row;
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if ($op == 'delete') {
+    $id         = $_GET['id'];
+    $sql1       = "delete from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    if ($q1) {
+        $sukses = "Berhasil hapus data";
+    } else {
+        $error  = "Gagal melakukan delete data";
     }
-    mysqli_free_result($result);
 }
+if ($op == 'edit') {
+    $id         = $_GET['id'];
+    $sql1       = "select * from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $nim        = $r1['nim'];
+    $nama       = $r1['nama'];
+    $alamat     = $r1['alamat'];
+    $fakultas   = $r1['fakultas'];
 
-// 5. tutup koneksi mysql
-mysqli_close($con);
-
-foreach($mahasiswa as $value){
-    echo $value["nama"];
+    if ($nim == '') {
+        $error = "Data tidak ditemukan";
+    }
 }
+if (isset($_POST['simpan'])) { //untuk create
+    $nim        = $_POST['nim'];
+    $nama       = $_POST['nama'];
+    $alamat     = $_POST['alamat'];
+    $fakultas   = $_POST['fakultas'];
 
+    if ($nim && $nama && $alamat && $fakultas) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update mahasiswa set nim = '$nim',nama='$nama',alamat = '$alamat',fakultas='$fakultas' where id = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1   = "insert into mahasiswa(nim,nama,alamat,fakultas) values ('$nim','$nama','$alamat','$fakultas')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Mahasiswa</title>
-</head>
-<body>
-    <h1>Data Mahasiswa</h1>
-    <a href="insert.php">Add Data</a>
-    <table border="1" style="width:100%;">
-        <tr>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Tempat Lahir</th>
-            <th>Action</th>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px
+        }
 
-        </tr>
-        <?php foreach($mahasiswa as $value): ?>
-        <tr>
-            <td><?php echo $value["nim"]; ?></td>
-            <td><?php echo $value["nama"]; ?></td>
-            <td><?php echo $value["tempat_lahir"]; ?></td>
-            <td>
-                <a href="<?php echo "update.php?id=".$value["id"]; ?>"> Edit</a>
-                <a href="<?php echo "delete.php?id=".$value["id"]; ?>"">Delete</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Create / Edit Data
+            </div>
+            <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php"); //5 : detik
+                }
+                ?>
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");
+                }
+                ?>
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nim" name="nim" value="<?php echo $nim ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $nama ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="alamat" name="alamat" value="<?php echo $alamat ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="fakultas" class="col-sm-2 col-form-label">Fakultas</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" name="fakultas" id="fakultas">
+                                <option value="">- Pilih Fakultas -</option>
+                                <option value="saintek" <?php if ($fakultas == "fmipa") echo "selected" ?>>FISIP</option>
+                                <option value="soshum" <?php if ($fakultas == "fisip") echo "selected" ?>>FMIPA</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <!-- untuk mengeluarkan data -->
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Mahasiswa
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Fakultas</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql2   = "select * from mahasiswa order by id desc";
+                        $q2     = mysqli_query($koneksi, $sql2);
+                        $urut   = 1;
+                        while ($r2 = mysqli_fetch_array($q2)) {
+                            $id         = $r2['id'];
+                            $nim        = $r2['nim'];
+                            $nama       = $r2['nama'];
+                            $alamat     = $r2['alamat'];
+                            $fakultas   = $r2['fakultas'];
+
+                        ?>
+                            <tr>
+                                <th scope="row"><?php echo $urut++ ?></th>
+                                <td scope="row"><?php echo $nim ?></td>
+                                <td scope="row"><?php echo $nama ?></td>
+                                <td scope="row"><?php echo $alamat ?></td>
+                                <td scope="row"><?php echo $fakultas ?></td>
+                                <td scope="row">
+                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
+                                    <a href="index.php?op=delete&id=<?php echo $id ?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
